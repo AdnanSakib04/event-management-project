ee75dfe012f3dc2f6148953edb4b02be998d906c
AdnanSakib04 <navidadnan04@gmail.com>
2023-10-08 12:24:57 +0600

    sponsors section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./Banner";
 import Services from "./Services";
+import Sponsors from "./Sponsors";
 
 const Home = () => {
     return (
@@ -8,6 +9,9 @@ const Home = () => {
             <Banner></Banner>
             <div className=" max-w-7xl mx-auto">
                 <Services></Services>
+            </div>
+            <div className=" max-w-7xl mx-auto">
+                <Sponsors></Sponsors>
             </div>
         </div>
     );
---
+++ src/pages/Home/Sponsors.jsx
@@ -0,0 +1,20 @@
+
+const Sponsors = () => {
+    return (
+        <div>
+            <h1 className=" text-5xl font-bold text-center mb-8">Our Sponsors</h1>
+
+
+            <div className=" card-body grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  justify-items-center gap-y-4">
+               <img className="w-3/4 md:w-auto h-[50px]  md:h-3/4"  src="https://i.ibb.co/34crFJq/logo2.jpg" alt="" />
+                <img className="w-3/4 md:w-auto h-[50px] md:h-3/4" src="https://i.ibb.co/2jpDZKd/logo1.jpg" alt="" />
+                <img className="w-3/4 md:w-auto h-[50px] md:h-3/4" src="https://i.ibb.co/DRgCFvG/logo5.jpg" alt="" />
+                <img className="w-1/2 md:w-auto h-[50px]  md:h-3/4" src="https://i.ibb.co/rkD6s94/logo6.jpg" alt="" />
+                <img className="w-1/2 md:w-auto h-[50px] md:h-3/4" src="https://i.ibb.co/9twM0JJ/logo4.jpg" alt="" />
+                <img className="w-1/2 md:w-auto h-[50px] md:h-3/4" src="https://i.ibb.co/MpZcvp1/logo3.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Sponsors;
