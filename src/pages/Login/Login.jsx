cd8008e09e0c6a6bfff248c92cb5efd98c2d26c7
AdnanSakib04 <navidadnan04@gmail.com>
2023-10-09 15:24:33 +0600

    login page updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -29,29 +29,31 @@ const Login = () => {
             })
     }
     return (
-        <div className=" max-w-7xl mx-auto">
-            <div>
-                <h2 className=" text-3xl mt-8 text-center">Login</h2>
-                <form onSubmit={handleLogin} className="md:w-1/2 mx-auto card-body">
+        <div className=" max-w-7xl mx-auto mb-40 ">
+            <div className="  p-4 mt-8 rounded-3xl ">
+                
+                <form onSubmit={handleLogin} className="md:w-1/2 mx-auto card-body bg-white rounded-3xl">
+                <h2 className="text-4xl font-bold lg:text-5xl mt-8 text-center"><span className="text-green-600">Please</span> Login</h2>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text font-medium text-[18px]">Email</span>
                         </label>
-                        <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        <input type="email" placeholder="email" name="email" className="input input-bordered  bg-base-200" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text font-medium text-[18px]">Password</span>
                         </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        <input type="password" name="password" placeholder="password" className="input input-bordered  bg-base-200" required />
                        
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+                        <button className="btn bg-green-300 font-bold text-xl">Login</button>
                     </div>
+                    <p className=" text-center mt-6 text-xl font-medium">Do not have an account? <Link className=" text-green-600 font-bold" to={'/register'}>Register</Link></p>
                 </form>
-                <p className=" text-center mt-4">Do not have an account? <Link className=" text-blue-600 font-bold" to={'/register'}>Register</Link></p>
-            </div>
+                
+                </div>
         </div>
     );
 };
--- src/pages/PastEventCard.jsx
+++ src/pages/PastEventCard.jsx
@@ -5,31 +5,31 @@ import { MdOutlineLocationOn } from "react-icons/md";
 const PastEventCard = ({ pastEvent }) => {
     const { id, description, title, photo, date, people, location } = pastEvent;
     return (
-        <div className={'card card-compact w-[400px]  border shadow-lg'}>
+        <div className={'card card-compact w-[400px]  border shadow-lg bg-white'}>
             <figure className="h-[226px]">
                 <img src={photo} alt="" />
             </figure>
 
             <div className="card-body text-black ">
-                <h2 className={'card-title font-semibold text-xl'}>{title}</h2>
+                <h2 className={'card-title font-bold text-xl text-green-600'}>{title}</h2>
                 <div className="flex items-center gap-2">
-                    <h2 className="text-base"><AiOutlineNumber></AiOutlineNumber></h2>
-                    <h3 className="text-base ">{people}</h3>
+                    <h2 className="text-base text-green-600"><AiOutlineNumber></AiOutlineNumber></h2>
+                    <h3 className="text-base font-medium">{people}</h3>
                 </div>
 
                 <div className="flex items-center gap-2">
-                    <h2 className="text-base"><MdOutlineLocationOn></MdOutlineLocationOn></h2>
-                    <h3 className="text-base ">{location}</h3>
+                    <h2 className="text-base text-green-600"><MdOutlineLocationOn></MdOutlineLocationOn></h2>
+                    <h3 className="text-base font-medium">{location}</h3>
                 </div>
 
                 <div className="flex items-center gap-2">
-                    <h2 className="text-base"><CiCalendarDate></CiCalendarDate></h2>
-                    <h3 className="text-base ">{date}</h3>
+                    <h2 className="text-base text-green-600"><CiCalendarDate></CiCalendarDate></h2>
+                    <h3 className="text-base font-medium">{date}</h3>
                 </div>
 
                 <div className="flex items-start gap-2">
-                    <h2 className=" text-base"><TbFileDescription></TbFileDescription></h2>
-                    <p className=" text-base text-justify">{description}</p>
+                    <h2 className=" text-base text-green-600"><TbFileDescription></TbFileDescription></h2>
+                    <p className=" text-base text-justify text-gray-500">{description}</p>
                 </div>
 
 
