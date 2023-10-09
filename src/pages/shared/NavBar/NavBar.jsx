b3ebda902bed107bb82017f82e0cc82db27bd515
AdnanSakib04 <navidadnan04@gmail.com>
2023-10-09 17:27:25 +0600

    navbar updated

--- src/pages/shared/NavBar/NavBar.jsx
+++ src/pages/shared/NavBar/NavBar.jsx
@@ -13,57 +13,56 @@ const NavBar = () => {
     }
     const navLinks = <>
         <li><NavLink className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-green-500 font-bold underline" : ""} to="/">Home</NavLink></li>
+            isPending ? "pending" : isActive ? "text-black font-bold bg-base-200 lg:bg-green-300 p-2 rounded-lg" : ""} to="/">Home</NavLink></li>
         <li><NavLink className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-green-500 font-bold underline" : ""} to="/pastevents">Past Events</NavLink></li>
+            isPending ? "pending" : isActive ? "text-black font-bold bg-base-200 lg:bg-green-300 p-2 rounded-lg" : ""} to="/pastevents">Past Events</NavLink></li>
         <li><NavLink className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-green-500 font-bold underline" : ""} to="/ourspeakers">Our Speakers</NavLink></li>
+            isPending ? "pending" : isActive ? "text-black font-bold bg-base-200 lg:bg-green-300 p-2 rounded-lg" : ""} to="/ourspeakers">Our Speakers</NavLink></li>
         <li><NavLink className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-green-500 font-bold underline" : ""} to="/login">Login</NavLink></li>
+            isPending ? "pending" : isActive ? "text-black font-bold bg-base-200 lg:bg-green-300 p-2 rounded-lg" : ""} to="/login">Login</NavLink></li>
         <li><NavLink className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-green-500 font-bold underline" : ""} to="/register">Register</NavLink></li>
+            isPending ? "pending" : isActive ? "text-black font-bold bg-base-200 lg:bg-green-300 p-2 rounded-lg" : ""} to="/register">Register</NavLink></li>
     </>
     return (
-        <div className="bg-base-100">
+        <div className="bg-white">
             <div className="navbar  max-w-7xl mx-auto">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-white rounded-box w-52">
+                            {navLinks}
+                        </ul>
+                    </div>
+                    {/* <a className="btn btn-ghost normal-case text-xl">Event Management</a> */}
+                    <img className="h-1/2 w-1/2 md:h-[50px] md:w-[180px]" src="https://i.ibb.co/VW0HXqv/image.png" alt="" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="flex gap-4 px-1">
                         {navLinks}
                     </ul>
+
                 </div>
-                {/* <a className="btn btn-ghost normal-case text-xl">Event Management</a> */}
-                <img className="h-1/2 w-1/2 md:h-[50px] md:w-[180px]" src="https://i.ibb.co/VW0HXqv/image.png" alt="" />
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navLinks}
-                </ul>
-
-            </div>
-            <div className="navbar-end">
-                {
-                    user ?
-                        <div className="flex items-center gap-3">
-                            <p>{user?.displayName}</p>
-                            <label  className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 h-10 rounded-[40px] ">
-                                <img src={user?.photoURL} />
+                <div className="navbar-end">
+                    {
+                        user ?
+                            <div className="flex items-center gap-3">
+                                <p className=" font-medium">{user?.displayName}</p>
+                                <label className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 h-10 rounded-[40px] ">
+                                        <img src={user?.photoURL} />
+                                    </div>
+                                </label>
+                                <button onClick={handleSignOut} className="btn bg-green-300 font-bold">Log Out</button>
                             </div>
-                            </label>
-                            <button onClick={handleSignOut} className="btn bg-green-300 font-bold">Log Out</button>
-                        </div>
-
-                        :
-                        <Link to="/login">
-                            <button className="btn bg-green-500">Login</button>
-                        </Link>
-                }
+                            :
+                            <Link to="/login">
+                                <button className="btn bg-green-500">Login</button>
+                            </Link>
+                    }
+                </div>
             </div>
-        </div>
         </div>
     );
 };
